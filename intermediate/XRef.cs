d667c750b99dcacceb45e84955678306faa520ea
Ioannis Fourkiotis <jsstark@gmail.com>
2014-03-09 21:47:15 +0200

    XRef made static

--- intermediate/XRef.cs
+++ intermediate/XRef.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace dradis.intermediate
 {
-    public class XRef
+    public static class XRef
     {
         private static readonly int NAME_WIDTH = 16;
         private static readonly string NAME_FORMAT = "{0, -" + NAME_WIDTH.ToString() + "}";
@@ -16,30 +16,30 @@ namespace dradis.intermediate
         private static readonly int INDENT_WIDTH = NAME_WIDTH + LABEL_WIDTH;
         private static readonly string INDENT = new String(' ', INDENT_WIDTH);
 
-        public void Print(SymbolTableStack stack)
+        public static void Print(SymbolTableStack stack)
         {
             Console.WriteLine("\n===== CROSS-REFERENCE TABLE =====");
             PrintColumnHeadings();
             PrintSymbolTable(stack.GetLocal());
         }
 
-        private void PrintColumnHeadings()
+        private static void PrintColumnHeadings()
         {
             Console.WriteLine();
             Console.WriteLine(String.Format(NAME_FORMAT + "{1}", "Identifier", NUMBERS_LABEL));
             Console.WriteLine(String.Format(NAME_FORMAT + "{1}", "----------", NUMBERS_UNDERLINE));
         }
 
-        private void PrintSymbolTable(SymbolTable table)
+        private static void PrintSymbolTable(SymbolTable table)
         {
             var entries = table.GetEntries();
             foreach(var e in entries)
             {
                 var lines = e.GetLines();
-                Console.WriteLine(String.Format(NAME_FORMAT, e.Name));
+                Console.Write(NAME_FORMAT, e.Name);
                 foreach(var i in lines)
                 {
-                    Console.WriteLine(String.Format("{0, 3}", i));
+                    Console.Write(String.Format("{0, 3} ", i));
                 }
                 Console.WriteLine();
             }
