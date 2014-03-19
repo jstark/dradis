97fbd972cf83df32e7f61a7744ae9decb0722fb1
Ioannis Fourkiotis <jsstark@gmail.com>
2014-03-20 00:01:51 +0200

    CompoundParser inherits from NonTerminal parser.

--- frontend/CompoundStatementParser.cs
+++ frontend/CompoundStatementParser.cs
@@ -11,40 +11,26 @@ using System.Diagnostics.Contracts;
 
 namespace dradis.frontend
 {
-    public class CompoundStatementParser : MessageProducer
+    public class CompoundStatementParser : NonTerminalParser
     {
-        private Scanner scanner;
-        private SymbolTableStack symtabstack;
-
-        private CompoundStatementParser(Scanner s, SymbolTableStack stack)
-        {
-            scanner = s;
-            symtabstack = stack;
-        }
-
-        public ICodeNode Parse(Token token)
+        public override ICodeNode Parse(Token token)
         {
             Contract.Requires(token.TokenType == TokenType.BEGIN);
             // ignore argument !
-            token = scanner.GetNextToken();
+            token = InternalScanner.GetNextToken();
 
             // create the COMPOUND node.
             ICodeNode compound_node = ICodeFactory.CreateICodeNode(ICodeNodeType.COMPOUND);
 
             // parse the statement list terminated by the END token.
-            StatementParser statement_parser = StatementParser.CreateWithObservers(scanner, symtabstack, observers);
+            StatementParser statement_parser = StatementParser.CreateWithObservers(InternalScanner, SymTabStack, observers);
             statement_parser.ParseList(token, compound_node, TokenType.END, ErrorCode.MISSING_END);
             return compound_node;
         }
 
         public static CompoundStatementParser CreateWithObservers(Scanner s, SymbolTableStack stack, List<IMessageObserver> obl)
         {
-            var compnd = new CompoundStatementParser(s, stack);
-
-            foreach (var o in obl)
-                compnd.Add(o);
-
-            return compnd;
+            return NonTerminalParser.CreateWithObserver<CompoundStatementParser>(s, stack, obl);
         }
     }
 }
