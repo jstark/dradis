d523164f610e03283c73280aa944deb4a65e9c6d
Ioannis Fourkiotis <jsstark@gmail.com>
2014-03-20 00:01:07 +0200

    StatementParser inherits from NonTerminalParser

--- frontend/StatementParser.cs
+++ frontend/StatementParser.cs
@@ -9,29 +9,55 @@ using dradis.message;
 
 namespace dradis.frontend
 {
-    public class StatementParser : MessageProducer
+    public class StatementParser : NonTerminalParser
     {
-        private Scanner scanner;
-        private SymbolTableStack symtabstack;
+        // Synchronization set for starting a statement.
+        internal static HashSet<TokenType> STMT_START_SET;
 
-        private StatementParser(Scanner s, SymbolTableStack stack)
+        // Synchronization set for following a statement.
+        internal static HashSet<TokenType> STMT_FOLLOW_SET;
+
+        static StatementParser()
         {
-            scanner = s;
-            symtabstack = stack;
+            // 
+            STMT_START_SET = new HashSet<TokenType>();
+            STMT_START_SET.Add(TokenType.BEGIN);
+            STMT_START_SET.Add(TokenType.CASE);
+            STMT_START_SET.Add(TokenType.FOR);
+            STMT_START_SET.Add(TokenType.IF);
+            STMT_START_SET.Add(TokenType.REPEAT);
+            STMT_START_SET.Add(TokenType.WHILE);
+            STMT_START_SET.Add(TokenType.IDENTIFIER);
+            STMT_START_SET.Add(TokenType.SEMICOLON);
+
+            //
+            STMT_FOLLOW_SET = new HashSet<TokenType>();
+            STMT_FOLLOW_SET.Add(TokenType.SEMICOLON);
+            STMT_FOLLOW_SET.Add(TokenType.END);
+            STMT_FOLLOW_SET.Add(TokenType.ELSE);
+            STMT_FOLLOW_SET.Add(TokenType.UNTIL);
+            STMT_FOLLOW_SET.Add(TokenType.DOT);
         }
 
-        public ICodeNode Parse(Token token)
+        public override ICodeNode Parse(Token token)
         {
             ICodeNode node;
             switch (token.TokenType)
             {
                 case TokenType.BEGIN:
-                    CompoundStatementParser cmpnd_parser = CompoundStatementParser.CreateWithObservers(scanner, symtabstack, observers);
+                    CompoundStatementParser cmpnd_parser = 
+                        CompoundStatementParser.CreateWithObservers(InternalScanner, SymTabStack, observers);
                     node = cmpnd_parser.Parse(token);
                     break;
                 case TokenType.IDENTIFIER:
-                    AssignmentStatementParser assign_parser = AssignmentStatementParser.CreateWithObservers(scanner, symtabstack, observers);
+                    AssignmentStatementParser assign_parser =
+                        AssignmentStatementParser.CreateWithObservers(InternalScanner, SymTabStack, observers);
                     node = assign_parser.Parse(token);
+                    break;
+                case TokenType.REPEAT:
+                    RepeatStatementParser repeat_parser =
+                        RepeatStatementParser.CreateWithObservers(InternalScanner, SymTabStack, observers);
+                    node = repeat_parser.Parse(token);
                     break;
                 default:
                     node = ICodeFactory.CreateICodeNode(ICodeNodeType.NO_OP);
@@ -45,33 +71,35 @@ namespace dradis.frontend
 
         public void ParseList(Token token, ICodeNode parent, TokenType terminator, ErrorCode error)
         {
+            var terminator_set = new HashSet<TokenType>(STMT_START_SET);
+            terminator_set.Add(terminator);
+
             while(!token.IsEof && token.TokenType != terminator)
             {
                 // parse a statement. The parent node adopts the statement node.
                 ICodeNode statement_node = Parse(token);
                 parent.Add(statement_node);
-                token = scanner.CurrentToken;
+                token = InternalScanner.CurrentToken;
 
                 // look for the semicolon between the statements.
                 if (token.TokenType == TokenType.SEMICOLON)
                 {
-                    token = scanner.GetNextToken();
+                    token = InternalScanner.GetNextToken();
                 }
-                else if (token.TokenType == TokenType.IDENTIFIER)
+                else if (STMT_START_SET.Contains(token.TokenType))
                 {
                     ErrorHandler.Flag(token, ErrorCode.MISSING_SEMICOLON, this);
                 }
-                else if (token.TokenType != terminator)
-                {
-                    ErrorHandler.Flag(token, ErrorCode.UNEXPECTED_TOKEN, this);
-                    token = scanner.GetNextToken();
-                }
+
+                // Synchronize at the start of the next statement
+                // or at the terminator.
+                token = Parser.Synchronize(terminator_set, InternalScanner, this);
             }
 
             // look for the terminator token
             if (token.TokenType == terminator)
             {
-                token = scanner.GetNextToken();
+                token = InternalScanner.GetNextToken();
             } else
             {
                 ErrorHandler.Flag(token, error, this);
@@ -80,12 +108,7 @@ namespace dradis.frontend
 
         public static StatementParser CreateWithObservers(Scanner s, SymbolTableStack stack, List<IMessageObserver> obl)
         {
-            var stmnt_parser = new StatementParser(s, stack);
-
-            foreach (var o in obl)
-                stmnt_parser.Add(o);
-
-            return stmnt_parser;
+            return NonTerminalParser.CreateWithObserver<StatementParser>(s, stack, obl);
         }
 
         private void SetLineNumber(ICodeNode node, int line)
